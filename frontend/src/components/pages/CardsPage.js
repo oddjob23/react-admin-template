bdf570ac8e2ba32596ead7f0e14df0270f7117b6
Milos Karaklajic <karaklajicmilos94@gmail.com>
2020-10-09 09:45:51 +0200

    minor fixes

--- frontend/src/components/pages/CardsPage.js
+++ frontend/src/components/pages/CardsPage.js
@@ -1,11 +1,9 @@
 import React from "react";
 import Card from "../Card";
 import img from "../../images/card_image_one.jpg";
-// import img2 from "../../images/card_image_two.jpg";
+import img2 from "../../images/card_image_two.jpg";
 import img3 from "../../images/card_image_three.jpg";
-
-const types = ["default"];
-const imagePositions = ["top", "bottom", "middle", "left", "right"];
+import img4 from "../../images/card_image_four.jpg";
 
 const CardsPage = () => {
   const cards = [
@@ -19,33 +17,54 @@ const CardsPage = () => {
     {
       type: "title-top",
       img: {
-        src: img3,
+        src: img2,
         description: "title-top",
       },
     },
     {
       type: "title-top-subtitle",
       img: {
-        src: img,
+        src: img3,
         description: "title-top",
       },
     },
     {
       type: "image-top-subtitle",
       img: {
-        src: img3,
+        src: img4,
         description: "default",
       },
     },
   ];
   return (
-    <div className="row">
-      {cards.map((card, index) => (
-        <div className="col-xl-3" key={index}>
-          <Card {...card} />
+    <>
+      <div className="row">
+        <div className="col-12">
+          <div className="page-title d-flex align-items-center justify-content-between">
+            <h4 className="mb-0 font-size-18">Cards</h4>
+            <div className="page-title-right">
+              <nav className="" aria-label="breadcrumb">
+                <ol className="breadcrumb m-0">
+                  <li className="breadcrumb-item">
+                    <a href="#">UI Elements</a>
+                  </li>
+                  <li className="active breadcrumb-item" aria-current="page">
+                    <a href="#r">Cards</a>
+                  </li>
+                </ol>
+              </nav>
+            </div>
+          </div>
         </div>
-      ))}
-    </div>
+      </div>
+      <div className="row">
+        {cards.map((card, index) => (
+          <div className="col-xl-3" key={index}>
+            <Card {...card} />
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
